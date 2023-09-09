0fc1ce58735ff7f52822362e6254a8ccce490c69
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2023-09-09 17:53:41 -0600

    Easy part done

--- lab1/lab1exe_F.c
+++ lab1/lab1exe_F.c
@@ -11,11 +11,32 @@
 const double G = 9.8;   /* gravitation acceleration 9.8 m/s^2 */
 const double PI = 3.141592654;
 
-void create_table(double v);
-double Projectile_travel_time(double a, double v);
-double Projectile_travel_distance(double a, double v);
+void create_table(double v){
+
+}
+
+
+double Projectile_travel_time(double a, double v){
+    /*Calculates projectile distance given the angle and a velocity with the formula given in the assignment
+    *Returns said time
+    */
+    double time = 2*v*sin(degree_to_radian(a))/G;
+    return time;
+}
+
+double Projectile_travel_distance(double a, double v){
+    /*Calculates projectile distance given the angle and a velocity with the formula given in the assignment
+    *Returns said distance
+    */
+    double distance = pow(v, 2)/G*sin(2*degree_to_radian(a));
+    return distance;
+}
+
 double degree_to_radian(double d){
-    
+    /*Converts from degrees to radians for sin() function
+    *Returns the angle in radians
+    */
+    double radian = d*((2*PI)/360);
 }
 
 int main(void)
