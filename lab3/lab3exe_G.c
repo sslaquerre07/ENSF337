56ec5c325062d7737a0a27dbb58d9ed545dd34ca
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2023-09-21 09:41:43 -0600

    Lab 3 File imported

---
+++ lab3/lab3exe_G.c
@@ -0,0 +1,40 @@
+/* lab3exe_G.c
+ * ENSF 337 Lab 3 Exercise  G
+ */
+
+#include <stdio.h>
+
+void funct (const char x [8], char y[], int n);
+
+int main()
+{
+    // It is unusual to intialize a string with '\0' in the middle of string,
+    // but it's allowed and we did it delibrately for the educational pruposes.
+    const char* sc = "\n\0map";
+    
+    char sa[5] = {'\0'};
+    
+    while (*sc) sc++;
+    
+    sc += 3;
+    
+    // Point One
+    
+    funct(sc, sa, &sa[5] - &sa[0]);
+    
+    return 0;
+}
+
+void funct (const char x[8], char y[], int n)
+{
+    
+    while (n ) {
+        y[0] = x[0];
+        n--;
+        x--;
+        y++;
+    }
+    
+    // Point Two
+    
+}
