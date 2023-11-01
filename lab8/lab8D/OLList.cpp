b3955c359b90a1e41927fe8dd3d0da2b4608016a
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2023-11-01 14:49:27 -0600

    Formatting for lab 8 done

---
+++ lab8/lab8D/OLList.cpp
@@ -0,0 +1,111 @@
+// OLList.cpp
+// ENSF 337 Fall 2023 Lab 8
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+#include "OLList.h"
+
+OLList::OLList()
+: headM(0)
+{
+}
+
+OLList::OLList(const OLList& source)
+{
+    copy(source);
+}
+
+OLList& OLList::operator =(const OLList& rhs)
+{
+    if (this != &rhs) {
+        destroy();
+        copy(rhs);
+    }
+    return *this;
+}
+
+OLList::~OLList()
+{
+    destroy();
+}
+
+void OLList::print() const
+{
+    cout << '[';
+    if (headM != 0) {
+        cout << ' ' << headM->item;
+        for (const Node *p = headM->next; p != 0; p = p->next)
+            cout << ", " << p->item;
+    }
+    cout << " ]\n";
+}
+
+void OLList::insert(const ListItem& itemA)
+{
+    Node *new_node = new Node;
+    new_node->item = itemA;
+    
+    if (headM == 0 || itemA <= headM->item ) {
+        new_node->next = headM;
+        headM = new_node;
+        // point one
+    }
+    else {
+        Node *before = headM;      // will point to node in front of new node
+        Node *after = headM->next; // will be 0 or point to node after new node
+        while(after != 0 && itemA > after->item) {
+            before = after;
+            after = after->next;
+        }
+        new_node->next = after;
+        before->next = new_node;
+        // point two
+    }
+}
+
+void OLList::remove(const ListItem& itemA)
+{
+    // if list is empty, do nothing
+    if (headM == 0 || itemA < headM->item)
+        return;
+    
+    Node *doomed_node = 0;
+    
+    if (itemA == headM->item) {
+        doomed_node = headM;
+        headM = headM->next;
+    }
+    else {
+        Node *before = headM;
+        Node *maybe_doomed = headM->next;
+        while(maybe_doomed != 0 && itemA > maybe_doomed->item) {
+            before = maybe_doomed;
+            maybe_doomed = maybe_doomed->next;
+        }
+        // point three
+    }
+    // the remaining part of this function is missing. As part of exercise A
+    // students are supposed to complete the rest of the definition of this function.
+}
+
+void OLList::destroy()
+{
+    
+    // this function is INCOMPLETE, and it is part of an exercise in the next lab (lab9)
+    // For lab 8 don't worry about this function
+    cout << "--program is terminated.\n";
+    headM = 0;
+}
+
+void OLList::copy(const OLList& source)
+{
+    // this function is INCOMPLETE, and it is part of an exercise in the next lab (lab9)
+    // For lab 8 don't worry about this function
+    (void) source;
+    
+    cout << "--program is terminated.\n";
+    exit(1);
+}
+
+
