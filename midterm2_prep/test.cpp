f6897fdd241678a2157cf96b9742104948f98311
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2023-11-17 16:22:52 -0700

    general changes

--- midterm2_prep/test.cpp
+++ midterm2_prep/test.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 #include <vector>
 #include <string>
+#include <iomanip>
 
 int main(){
     string s1("Hello World");
+    double a1 = 3.147367457736;
     s1.push_back('!');
-    cout << s1;
+    cout << s1 << endl;
+    cout << setiosflags(ios::scientific) << setprecision(2) << a1 << endl;
+    cout << setw(20) << s1 << setw(20) << a1;
 }
