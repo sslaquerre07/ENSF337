d9528beab0dafbacba06da046c5e85d8cc43a692
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2023-11-17 15:57:18 -0700

    general changes

--- midterm2_prep/test.cpp
+++ midterm2_prep/test.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
 using namespace std;
-
-int Search(int key, const int* array, int low, int high);
+#include <vector>
+#include <string>
 
 int main(){
-    int array[] = {1,2,3,4,5};
-    cout << Search(6, array, 0, 4);
-
-}
-
-int Search(int key, const int* array, int low, int high){
-    if(low>high)
-        return -1;
-    else{
-        if(array[low]==key)
-            return low;
-        else
-            Search(key, array, low+1, high);    
-    }
+    string s1("Hello World");
+    s1.push_back('!');
+    cout << s1;
 }
