ee195e5e724ebbc40808eb1ee44a42146bcaba57
sslaquerre07 <125930722+sslaquerre07@users.noreply.github.com>
2023-11-18 19:56:58 -0700

    general changes

--- midterm2_prep/test.cpp
+++ midterm2_prep/test.cpp
@@ -4,11 +4,11 @@ using namespace std;
 #include <string>
 #include <iomanip>
 
+
+
 int main(){
-    string s1("Hello World");
-    double a1 = 3.147367457736;
-    s1.push_back('!');
-    cout << s1 << endl;
-    cout << setiosflags(ios::scientific) << setprecision(2) << a1 << endl;
-    cout << setw(20) << s1 << setw(20) << a1;
+    const char* s = "ON TIME";
+    const char ** m = &s;
+    cout << *(*m+1);
 }
+
